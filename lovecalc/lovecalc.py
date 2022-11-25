aefe6442327140c1a2afcfac4333ff5a1c55a1ca
phantom6309 <113944014+phantom6309@users.noreply.github.com>
2022-11-25 19:12:33 +0300

    Update lovecalc.py

--- lovecalc/lovecalc.py
+++ lovecalc/lovecalc.py
@@ -26,5 +26,6 @@ class LoveCalc(commands.Cog):
             4: ' "İki aşık" \N{GROWING HEART}\N{GROWING HEART}'
         }
 		await ctx.send(
-	**{p1.display_name}** ve **{p2.display_name}** 'f' {love}% uyumlu! {love_dict[love//25]}'
+			 **{p1.display_name}** and **{p2.display_name}** '
+			f'are {love}% compatible! {love_dict[love//25]}'
 		)
